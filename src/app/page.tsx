297249203630544d2c63b8d984bffbb43d7c61ac
troybedingfield <troybedingfield@gmail.com>
2024-07-26 23:28:30 -0700

    viewport removed from page.tsx in src/app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
-import type { Viewport } from 'next'
-
-export const viewport: Viewport = {
-  initialScale: 1,
-  width: "device-width",
-  maximumScale: 1,
-};
 
 export default function Home() {
   return (
