c185af429d240728806959810d228e9d1bc7d601
troybedingfield <troybedingfield@gmail.com>
2024-07-26 22:54:08 -0700

    viewport update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
+import type { Viewport } from 'next'
+
+export const viewport: Viewport = {
+  initialScale: 1,
+  width: "device-width",
+  maximumScale: 1,
+};
 
 export default function Home() {
   return (
