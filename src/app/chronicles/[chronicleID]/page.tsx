cbcf1a620cb7f8bc1e6aa348a08b901f451a0bb9
troybedingfield <troybedingfield@gmail.com>
2024-07-30 18:34:18 -0700

    [chronicleID] and chronicles/page.tsx update to fix build

---
+++ src/app/chronicles/[chronicleID]/page.tsx
@@ -0,0 +1,8 @@
+export default async function Chroncile({ params, ...props }: { params: { chronicleID: number } }) {
+    const { } = props;
+
+    return (
+        <>
+        </>
+    )
+}
--- src/app/chronicles/page.tsx
+++ src/app/chronicles/page.tsx
@@ -2,13 +2,9 @@ import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import CreateChronicle from "./CreateChronicle/CreateChronicle";
 
-export default async function Chronicles({ ...props }) {
-    const { } = props;
+export default async function Chronicles() {
 
     const supabase = createClient()
-
-    // const { data: chronicles } = await supabase.from("chronicles").select().order('id', { ascending: true });
-
 
     const { data, error } = await supabase.auth.getUser()
     if (error || !data?.user) {
@@ -17,16 +13,9 @@ export default async function Chronicles({ ...props }) {
 
     return (
         <div className='container flex flex-wrap gap-8'>
-            {/* {characters?.map(({ char_id, id, charName: name, charChronicle: chronicle }, character) => {
-                    return (
 
-                        <Character key={char_id} id={id} name={name} chronicle={chronicle} />
-
-
-                    )
-                })} */}
             <div className='container flex items-center content-center justify-center min-h-40 max-w-36 flex-col gap-2 border border-slate-300 rounded-lg'>
-                {/* <CreateCharacter user={data.user.id} /> */}
+
                 <CreateChronicle user={data.user.id} />
             </div>
         </div>
