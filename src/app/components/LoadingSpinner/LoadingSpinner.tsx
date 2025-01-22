ce933d80493652bf2867dc41b7d0fc6a41dbd7e0
troybedingfield <troybedingfield@gmail.com>
2025-01-22 11:39:37 -0800

    fix loading spinner to align center with viewport height and width

--- src/app/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/app/components/LoadingSpinner/LoadingSpinner.tsx
@@ -4,6 +4,8 @@ export default function LoadingSpinner({ ...props }) {
     const { } = props;
 
     return (
-        <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+        <div className='container relative flex justify-center items-center h-[inherit]'>
+            <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+        </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,14 @@ import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
 
 export default function Home() {
   return (
+
     <Suspense fallback={<LoadingSpinner />}>
       <div className="container flex flex-col w-full h-full items-center justify-center">
         <h1>Welcome to your Unlife</h1>
         <Link href="/login" className="">Login</Link>
       </div>
     </Suspense>
+
+
   );
 }
