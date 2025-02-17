352195250849fd0692aeaddf15c2b8d4475facdd
troybedingfield <troybedingfield@gmail.com>
2025-02-16 19:48:04 -0800

    reset password login page form and updated actions

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -4,6 +4,19 @@ import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
 import { createClient } from '@/utils/supabase/server'
+import { NextRequest } from 'next/server'
+
+
+export async function checkAuth() {
+    const supabase = createClient()
+    const { data, error } = await supabase.auth.getUser()
+    if (error || !data?.user) {
+        // console.log(error);
+    }
+    if (!error) {
+        redirect('/dashboard')
+    }
+}
 
 export async function login(formData: FormData) {
     const supabase = createClient()
@@ -44,4 +57,33 @@ export async function signup(formData: FormData) {
 
     revalidatePath('/dashboard', 'layout')
     redirect('/dashboard')
+}
+
+
+export async function sendResetPassword(formData: FormData, request: NextRequest) {
+    const supabase = createClient()
+
+    // // type-casting here for convenience
+    // // in practice, you should validate your inputs
+    const data = {
+        email: formData.get('email') as string,
+    }
+
+    // const { error } = await supabase.auth.signInWithPassword(data)
+
+    // if (error) {
+    //     redirect('/error')
+    // }
+
+    // revalidatePath('/dashboard', 'layout')
+    // redirect('/dashboard')
+    console.log(data.email)
+
+    try {
+        const { data: resetData, error } = await supabase.auth.resetPasswordForEmail(data.email, {
+            redirectTo: `https://wod.robotknifefight.com/reset`
+        })
+    } catch (error) {
+        console.log(error)
+    }
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,36 +1,67 @@
-import { createClient } from '@/utils/supabase/server'
+'use client'
+import { createClient } from '@/utils/supabase/client'
 import Button from '../components/Button/Button'
 import LoadingSpinner from '../components/LoadingSpinner/LoadingSpinner'
-import { login, signup } from './actions'
+import { checkAuth, login, sendResetPassword, signup } from './actions'
 import './page.scss'
 import { redirect } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
 
-export default async function LoginPage() {
+export default function LoginPage() {
 
-    const supabase = createClient()
+    const [resetPassword, setResetPassword] = useState<boolean>(false);
+    const [success, setSuccess] = useState<boolean>(false);
 
-    const { data, error } = await supabase.auth.getUser()
-    if (error || !data?.user) {
-        // console.log(error);
-    }
-    if (!error) {
-        redirect('/dashboard')
-    }
+    useEffect(() => {
+        checkAuth();
+
+    }, [])
+
+
+
 
 
     return (
-        <div className='auth-container content-center items-center'>
-            <div className='authFormContainer mx-auto '>
-                <form>
-                    <label htmlFor="email">Email:</label>
-                    <input id="email" name="email" type="email" required />
-                    <label htmlFor="password">Password:</label>
-                    <input id="password" name="password" type="password" required />
-                    <Button formAction={login}>Log in</Button>
-                    {/* <Button formAction={signup}>Sign up</Button> */}
-                </form>
-            </div>
-            {/* <LoadingSpinner /> */}
-        </div>
+        <>
+
+            {!resetPassword && <div className='auth-container content-center items-center'>
+                <div className='flex relative flex-col w-full justify-items-center'>
+                    <div className='authFormContainer mx-auto '>
+                        <form>
+                            <label htmlFor="email">Email:</label>
+                            <input id="email" name="email" type="email" required />
+                            <label htmlFor="password">Password:</label>
+                            <input id="password" name="password" type="password" required />
+                            <Button formAction={login}>Log in</Button>
+                            {/* <Button formAction={signup}>Sign up</Button> */}
+                        </form>
+                    </div>
+                    <p className='hover:underline w-auto mx-auto' onClick={() => setResetPassword(!resetPassword)}>{resetPassword ? "login" : "Reset Password"}</p>
+                </div>
+                {/* <LoadingSpinner /> */}
+            </div>}
+            {resetPassword &&
+                <>
+                    <div className='auth-container content-center items-center'>
+                        <div className='flex relative flex-col w-full justify-items-center'>
+                            <div className='authFormContainer mx-auto '>
+                                <form>
+                                    <label htmlFor="email">Email:</label>
+                                    <input id="email" name="email" type="email" required />
+                                    <Button formAction={sendResetPassword}>Reset My Password</Button>
+                                    {/* <Button formAction={signup}>Sign up</Button> */}
+                                    {success && <p className='bg-green-100 text-green-600 px-2 rounded'>Success! Check your email to reset your password.</p>}
+                                </form>
+                            </div>
+                            <p className='hover:underline w-auto mx-auto' onClick={() => setResetPassword(!resetPassword)}>{resetPassword ? "login" : "Reset Password"}</p>
+                        </div>
+                        {/* <LoadingSpinner /> */}
+                    </div>
+
+                </>
+            }
+
+        </>
     )
 }
